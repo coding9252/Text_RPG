31ed000fe34d29716dd98c1823875556f8728613
Gahyeon <coding9252@naver.com>
2023-11-28 17:41:26 +0900

    Init

--- app/src/main/java/com/leegahyeon/text_rpg/WorldMain.kt
+++ app/src/main/java/com/leegahyeon/text_rpg/WorldMain.kt
@@ -34,7 +34,7 @@ fun main() {
         println("==================${worldName}에 오신것을 환영합니다==================")
         println("당신의 정보는 다음과 같습니다.")
         println("이름: ${myName}입니다.")
-        println("나이: ${myAge}입니다.")
+        println("나이: ${myAge}살 입니다.")
         println("직업: ${myJob}입니다.")
         println("모험을 떠나 볼까요?")
     }
