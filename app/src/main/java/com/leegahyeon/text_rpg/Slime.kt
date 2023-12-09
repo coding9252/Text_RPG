6e744c942c8185b77751e1de6e3213f73205e607
Gahyeon <coding9252@naver.com>
2023-12-09 23:29:45 +0900

    안정성 개선

--- app/src/main/java/com/leegahyeon/text_rpg/Slime.kt
+++ app/src/main/java/com/leegahyeon/text_rpg/Slime.kt
@@ -1,22 +1,22 @@
 package com.leegahyeon.text_rpg
 
-class Slime : Monster, GreenSlimeSkill {    // Monster 클래스 상속 받고, GreenSlimeSkill 인터페이스도 받음
-    var name:String = ""
-    var color:String = ""
-    var height:Double = 0.0
-    var hp:Int = 0
-    var damage:Int = 0
+class Slime : Monster, GreenSlimeSkill {
+    var name:String
+    var color:String
+    var height:Double
+    var hp:Int
+    var damage:Int
 
     constructor(_name:String, _color:String, _height:Double, _hp:Int, _damage:Int) {
-
         name = _name
         color = _color
         height = _height
         hp = _hp
         damage = _damage
+        println("${name}슬라임 생성")
     }
 
-    override fun attack() {     // Monster 상속 받아서 attach 메소드를 오버라이딩.
+    override fun attack() {
         println("점성 공격!")
     }
 
@@ -24,7 +24,7 @@ class Slime : Monster, GreenSlimeSkill {    // Monster 클래스 상속 받고, 
         println("점프해서 내려찍기!")
     }
 
-    override fun poison() {     // GreenSlimeSkill 인터페이스를 받아서 color가 초록일 때 poison() 메소드를 사용할 수 있다.
+    override fun poison() {
         if(color == "초록") {
             println("초록 독 퍼뜨리기!")
         } else {
--- app/src/main/java/com/leegahyeon/text_rpg/Zombie.kt
+++ app/src/main/java/com/leegahyeon/text_rpg/Zombie.kt
@@ -1,22 +1,23 @@
 package com.leegahyeon.text_rpg
 
-class Zombie : Monster {    // Monster 클래스의 상속을 받음
-    var name:String = ""
-    var color:String = ""
-    var height:Double = 0.0
-    var hp:Int = 0
-    var damage:Int = 0
+class Zombie : Monster {
+    var name:String
+    var color:String
+    var height:Double
+    var hp:Int
+    var damage:Int
 
     constructor(_name:String, _color:String, _height:Double, _hp:Int, _damage:Int) {
-        println("${name}좀비 생성")
+//        println("${name}좀비 생성")
         name = _name
         color = _color
         height = _height
         hp = _hp
         damage = _damage
+        println("${name}좀비 생성")
     }
 
-    override fun attack() {    // Monster 클래스의 attack 메소드 오버라이딩
+    override fun attack() {
         println("물어뜯기 공격!")
     }
 
