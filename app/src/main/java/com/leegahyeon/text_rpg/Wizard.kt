6dd9fde96b979c0aee86ca483ea2b48472df03f9
Gahyeon <coding9252@naver.com>
2023-12-13 23:44:03 +0900

    효율성 개선1

--- app/src/main/java/com/leegahyeon/text_rpg/Wizard.kt
+++ app/src/main/java/com/leegahyeon/text_rpg/Wizard.kt
@@ -1,21 +1,23 @@
 package com.leegahyeon.text_rpg
 
 class Wizard : Character {
-    var name:String           // Archer와 같이 원래 다 초기화해줬었는데 어차피 생성자에서 받아와서 값을 초기화하니까 생략 가능
+    var name:String
     var age:Int
     var gender:String
     var money:Int
     var hp:Int
     var mp:Int
+    var weapons:MutableList<String>
 
     constructor(_name:String, _age:Int, _gender:String, _money:Int, _hp:Int, _mp:Int) {
+        weapons = mutableListOf<String>()
         name = _name
         age = _age
         gender = _gender
         money = _money
         hp = _hp
         mp = _mp
-        println("${name}마법사 생성")    // 이 println이 name = _name 위에 있었는데 name을 초기화하고 쓸 수 있기 때문에 아래로 보냄.
+        println("${name}마법사 생성")
     }
 
     override fun attack() {
